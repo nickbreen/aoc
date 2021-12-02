c544c7d151865dd0ba4b1e127e68852722e92fe3
Nicholas Breen <breenn@lmax.com>
2021-12-02 18:01:41 +1300

    rename data files

--- puzzle1.sh
+++ puzzle1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-./count_increases.awk < test.txt
-./count_increases.awk < data.txt
+./count_increases.awk < test.depths.txt
+./count_increases.awk < data.depths.txt
 
-./moving_sum.awk < test.txt | ./count_increases.awk
-./moving_sum.awk < data.txt | ./count_increases.awk
+./moving_sum.awk < test.depths.txt | ./count_increases.awk
+./moving_sum.awk < data.depths.txt | ./count_increases.awk
