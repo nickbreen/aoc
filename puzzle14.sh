52c6493bfcfd13643ce926c23c2ecc472849fe16
Nicholas Breen <breenn@lmax.com>
2021-12-14 21:20:33 +1300

    puzzle14 part2

--- puzzle14.sh
+++ puzzle14.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-./poly.awk -vrounds=1 test.poly.txt data.poly.txt
+./poly.awk -vrounds=10 test.poly.txt data.poly.txt
 
-./poly2.awk -vrounds=1 test.poly.txt data.poly.txt
-./poly2.awk -vrounds=10 test.poly.txt data.poly.txt
 ./poly2.awk -vrounds=40 test.poly.txt data.poly.txt
