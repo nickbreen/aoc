cd5cb3d3884beec0d6c1afa0706397b005c230b2
Nicholas Breen <nick@breen.kiwi>
2024-12-16 20:47:36 +1300

    d6p2 - wrong

--- d6/p2.py
+++ d6/p2.py
@@ -9,9 +9,12 @@ left = "<"
 right = ">"
 dirs = [up, down, left, right]
 next_dir = {up: right, right: down, down: left, left: up}
+prev_dir = {a:b for b,a in next_dir.items()}
 empty = "."
 obstacle = "#"
 patrolled = "X"
+
+print (sys.argv[1:])
 
 map = [list(line.strip()) for line in fileinput.input(files=sys.argv[1:])]
 
@@ -22,34 +25,33 @@ for y in range(len(map)):
             guard = (y,x,map[y][x])
             break
 
-#print ("\n".join(["".join(line) for line in map]))
-
+route = []
+obstacles = []
 try:
     while True: #prompt("Press Enter to continue...", True):
-        print (f"guard is at {guard}")
+        #print (f"guard is at {guard}")
+
+        # Wrong, needs to check that an obstacle is anywhere to the next_dir of the guard
+        if (guard[0], guard[1], next_dir[guard[2]]) in route:
+            obstacles += [(guard[0], guard[1], next_dir[guard[2]])]
+        route += [guard]
         map[guard[0]][guard[1]] = patrolled
         if guard[2] == up and map[guard[0]-1][guard[1]] != obstacle:
             guard = (guard[0]-1, guard[1], guard[2])
-        elif guard[2] == up:
-            guard = (guard[0], guard[1], next_dir[guard[2]])
         elif guard[2] == down and map[guard[0]+1][guard[1]] != obstacle:
             guard = (guard[0]+1, guard[1], guard[2])
-        elif guard[2] == down:
-            guard = (guard[0], guard[1], next_dir[guard[2]])
         elif guard[2] == left and map[guard[0]][guard[1]-1] != obstacle:
             guard = (guard[0], guard[1]-1, guard[2])
-        elif guard[2] == left:
-            guard = (guard[0], guard[1], next_dir[guard[2]])
         elif guard[2] == right and map[guard[0]][guard[1]+1] != obstacle:
             guard = (guard[0], guard[1]+1, guard[2])
-        elif guard[2] == right:
+        else:
             guard = (guard[0], guard[1], next_dir[guard[2]])
         map[guard[0]][guard[1]] = guard[2]
+
        #print ("\n".join(["".join(line) for line in map]))
 
 except IndexError:
     #print ("\n".join(["".join(line) for line in map]))
-    route = [1 for line in map for c in line if c == patrolled]
-    n = sum(route)
-    print (f"guard exited at {guard} after patrolling {n} squares")
+    n = sum([1 for line in map for c in line if c == patrolled])
+    print (f"guard exited at {guard} after patrolling {n} squares with {len(obstacles)} new loop obstacles: \n{obstacles}\n over route: \n{route}")
     exit(0)
